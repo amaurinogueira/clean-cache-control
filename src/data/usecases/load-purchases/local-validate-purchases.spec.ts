6efbb931da3cf17a6340eb0b35c842ba922ba2f7
Amauri Nogueira <50035391+amaurinogueira@users.noreply.github.com>
2020-09-20 18:50:12 -0300

    ensure LocalLoadPurchases has no side effects on validate if load succeds

--- src/data/usecases/load-purchases/local-validate-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-validate-purchases.spec.ts
@@ -1,5 +1,5 @@
 import { LocalLoadPurchases } from '@/data/usecases'
-import {  CacheStoreSpy } from '@/data/tests'
+import {  CacheStoreSpy,  getCacheExpirationDate} from '@/data/tests'
 
 
 type SutTypes = {
@@ -32,4 +32,14 @@ describe('LocalLoadPurchases', () => {
         expect(cacheStore.deleteKey).toBe('purchases')
     })
 
+    test('Should has no side effect if load succeeds', () => {
+        const currentDate = new Date()
+        const timestamp = getCacheExpirationDate(currentDate)
+        timestamp.setSeconds(timestamp.getSeconds() + 1)
+        const { cacheStore, sut }  = makeSut(timestamp)
+        cacheStore.fetchResult = {  timestamp  }
+        sut.validate()
+        expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch])
+        expect(cacheStore.fetchKey).toBe('purchases')
+    })
 })
